d2f640f60e198f1daef830c6994dd9ee1893d9f6
monirajan <a.monisha.1995@gmail.com>
2021-04-08 21:17:04 +0530

    Initial project commit

---
+++ src/main/java/com/spring/boot/springrestscheduler/SpringRestSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.spring.boot.springrestscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/boot/springrestscheduler/SpringRestSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.boot.springrestscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
